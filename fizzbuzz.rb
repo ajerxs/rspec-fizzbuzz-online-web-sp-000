f47379b877093ba9885371e4a0e2c2865588d0fb
Andrew Eames <ajerxs@gmail.com>
2020-10-23 23:07:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  if int % 5 == 0
     "Buzz"
-  elsif int % 15 == 0
+  if int % 15 == 0
     "FizzBuzz"
-  else
-    nil
 end
 
 fizzbuzz(3)
